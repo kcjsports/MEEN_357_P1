4bb18a12d95695e6b9eccdab907299b4460e2d36
kcjsports <kalen.jaroszewski@outlook.com>
2026-02-02 13:03:17 -0600

    Busy Work

---
+++ subfunctions.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def taudc_motor(omega: np.ndarray, motor:dict):
+  return tau
+
+def get_gear_ratio(speed_reducer: dict):
+  return Ng
+
+def get_mass(rover: dict):
+  return m
+
+def F_drive(omega: np.ndarray, rover: dict):
+  )return Fd
+
+def F_gravity(terrain_angle: np.ndarray, rover: dict, planet: dict):
+  return Fgt
+
+def F_rolling(omega: np.ndarray, terrain_angle: np.ndarray, rover: dict, planet: dict, Crr):
+  return Frr
+
+def F_net(omega: np.ndarray, terrain_angle: np.ndarray, rover: dict, planet: dict, Crr):
+  return Fnet
+
