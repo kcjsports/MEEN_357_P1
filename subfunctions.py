5a877143f90abef3502a68c1641ecfa34083d969
jamesszymczak <jamesszymczak@tamu.edu>
2026-02-08 17:45:16 -0600

    fixing subfunctions

--- subfunctions.py
+++ subfunctions.py
@@ -100,7 +100,7 @@ def F_rolling(omega: np.ndarray, terrain_angle: np.ndarray, rover: dict, planet:
      raise Exception("To steep")
   m = get_mass(rover)
   Ng = get_gear_ratio(rover["wheel_assembly"]["speed_reducer"])
-  Fn = m * planet["g_mars"] * np.cos(np.radians(terrain_angle))
+  Fn = m * planet["g"] * np.cos(np.radians(terrain_angle))
   Frr_simple = Crr * Fn
   Frr = special.erf(40 * omega * Ng * rover["wheel_assembly"]["wheel"]["radius"]) * Frr_simple
   return Frr
