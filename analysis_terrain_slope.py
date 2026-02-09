87f5269a1cc6562e03bfa6edc76e1b169f267a8f
jamesszymczak <jamesszymczak@tamu.edu>
2026-02-09 08:34:44 -0600

    terrain analysis should work now

--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -11,27 +11,22 @@ motor = sf.Marvin["rover"]["wheel_assembly"]["motor"]
 rover = sf.Marvin["rover"]
 planet = sf.Marvin["planet"]
 #variables
-omega = np.linspace(0, 3.8, 25) #array of our omega 
+omega = np.linspace(0, motor['speed_noload'], 25) #array of our omega 
 Crr = 0.15
 slope_array_deg = np.linspace(-15,35,25)
 v_max = np.ndarray(25)
 wheel_radius = sf.Marvin["rover"]["wheel_assembly"]["wheel"]["radius"]
-motor = sf.Marvin["rover"]["wheel_assembly"]["motor"]
 
-#Find where Fnet - tau/r = 0 for all values in slope array
-root = np.zeros(25)
 
-#NOT FINShed but i think it almost works
 for i in range(25):
     if f(0, slope_array_deg[i],rover, planet) * f(3.8, slope_array_deg[i],rover, planet) >= 0:
-        print("aga")
+        v_max[i] = motor['speed_noload'] * wheel_radius
     else:
-        root[i] = root_scalar(f,method='bisect', args=(slope_array_deg[i],rover, planet), bracket=[0,motor['speed_noload']]).root
-print(root)
+        v_max[i] = root_scalar(f,method='bisect', args=(slope_array_deg[i],rover, planet), bracket=[0,motor['speed_noload']]).root * wheel_radius
+print(v_max)
 
-
-# plt.plot(slope_array_deg, v_max)
-# plt.xlabel('Terrain Angle (degrees)')
-# plt.ylabel('Maximum Rover Velocity (m/s)')
-# plt.title('Rover Maximum Velocity vs Terrain Slope')
-# plt.show()
+plt.plot(slope_array_deg, v_max)
+plt.xlabel('Terrain Angle (degrees)')
+plt.ylabel('Maximum Rover Velocity (m/s)')
+plt.title('Rover Maximum Velocity vs Terrain Slope')
+plt.show()
