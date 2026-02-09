9003906b3f2a1afa307a5ef4f96dd2eda39ec667
jamesszymczak <jamesszymczak@tamu.edu>
2026-02-08 22:36:18 -0600

    fixed subfunctions, completed the graphing files

--- graphs_motor.py
+++ graphs_motor.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from subfunctions import taudc_motor, Marvin
+from subfunctions import tau_dcmotor, Marvin
 
 motor = Marvin["rover"]["wheel_assembly"]["motor"]
 N = 25 #our number of data point
 omega = np.linspace(0, motor["speed_noload"], N) #array of our omega 
 
-t = taudc_motor(omega,motor) #calulates the value of our tau values
+t = tau_dcmotor(omega,motor) #calulates the value of our tau values
 
 #defines and calulates power at each omega
 p = np.ndarray(N)
@@ -15,10 +15,26 @@ for i in  range(len(omega)):
 
 
 plt.rcParams["font.family"] = "serif"
-fig, ax = plt.subplots(3,1)
-ax[0].plot(t, omega); ax[0].set_title("Motor - Torque vs. Speed")
-ax[1].plot(t, p); ax[1].set_title("Motor - Torque vs. Power")
-ax[2].plot(omega, p); ax[2].set_title("Motor - Speed vs. Power")
 
+fig, axes = plt.subplots(3,1, figsize=(9,9))
+
+axes[0].plot(t, omega); axes[0].set_title("Motor - Torque vs. Speed")
+axes[1].plot(t, p); axes[1].set_title("Motor - Torque vs. Power")
+axes[2].plot(omega, p); axes[2].set_title("Motor - Speed vs. Power")
+
+axes[0].set_xlabel('Torque')
+axes[1].set_xlabel('Torque')
+axes[2].set_xlabel('Speed')
+
+axes[0].set_ylabel('Speed')
+axes[1].set_ylabel('Power')
+axes[2].set_ylabel('Power')
+
+
+axes[0].set_box_aspect(1)
+axes[1].set_box_aspect(1)
+axes[2].set_box_aspect(1)
+
+fig.tight_layout()
 
 plt.show()
--- graphs_sr.py
+++ graphs_sr.py
@@ -1,28 +1,48 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import subfunctions
-import graphs_motor
+from subfunctions import tau_dcmotor, get_gear_ratio, Marvin
 
-Marvin = subfunctions.Marvin
 motor = Marvin["rover"]["wheel_assembly"]["motor"]
-N = graphs_motor.N #our number of data point
-omega = np.linspace(0, motor["speed_noload"], N) #array of our omega
-t = subfunctions.taudc_motor(omega,motor) #calulates the value of our tau values
-Ng = subfunctions.get_gear_ratio(Marvin["rover"]["wheel_assembly"]["speed_reducer"])
-omega_out = omega / Ng
-tau_out = t * Ng
+N = 25 #our number of data point
+omega = np.linspace(0, motor["speed_noload"], N) #array of our omega 
+
+t = tau_dcmotor(omega,motor) #calulates the value of our tau values
+
+Ng = get_gear_ratio(Marvin["rover"]["wheel_assembly"]["speed_reducer"])
+
+omega_out = np.zeros(N)
+t_out = np.zeros(N)
+for i in range(N):
+  omega_out[i] = omega[i]/Ng
+  t_out[i] = t[i] * Ng
 
 #defines and calulates power at each omega
 p = np.ndarray(N)
 for i in  range(len(omega_out)):
-  p[i] = tau_out[i]*omega_out[i]
+  p[i] = t_out[i]*omega_out[i]
 
 
 plt.rcParams["font.family"] = "serif"
-fig, ax = plt.subplots(3,1)
-ax[0].plot(tau_out, omega_out); ax[0].set_title("Motor - Torque vs. Speed")
-ax[1].plot(tau_out, p); ax[1].set_title("Motor - Torque vs. Power")
-ax[2].plot(omega_out, p); ax[2].set_title("Motor - Speed vs. Power")
 
+fig, axes = plt.subplots(3,1, figsize=(9,9))
+
+axes[0].plot(t_out, omega_out); axes[0].set_title("Motor - Torque vs. Speed")
+axes[1].plot(t_out, p); axes[1].set_title("Motor - Torque vs. Power")
+axes[2].plot(omega_out, p); axes[2].set_title("Motor - Speed vs. Power")
+
+axes[0].set_xlabel('Torque')
+axes[1].set_xlabel('Torque')
+axes[2].set_xlabel('Speed')
+
+axes[0].set_ylabel('Speed')
+axes[1].set_ylabel('Power')
+axes[2].set_ylabel('Power')
+
+
+axes[0].set_box_aspect(1)
+axes[1].set_box_aspect(1)
+axes[2].set_box_aspect(1)
+
+fig.tight_layout()
 
 plt.show()
