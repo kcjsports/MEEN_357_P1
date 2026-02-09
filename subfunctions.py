a680e37642453462c8c9f35fdba18d34ffc6a001
jamesszymczak <jamesszymczak@tamu.edu>
2026-02-08 18:23:14 -0600

    Update subfunctions.py

--- subfunctions.py
+++ subfunctions.py
@@ -110,7 +110,7 @@ def F_rolling(omega: np.ndarray, terrain_angle: np.ndarray, rover: dict, planet:
   for i in range(omega.size):
     Fn = m * planet["g"] * np.cos(np.radians(terrain_angle[i]))
     Frr_simple = Crr * Fn
-    Frr[i] = erf(40 * omega[i] * Ng * rover["wheel_assembly"]["wheel"]["radius"]) * Frr_simple
+    Frr[i] = -1 * erf(40 * omega[i] * Ng * rover["wheel_assembly"]["wheel"]["radius"]) * Frr_simple
   return Frr
 #np.array(1)
 #print(F_rolling(np.array([1,2,3]), np.array([5,30,3]), Marvin["rover"], Marvin["planet"], 0.5))
