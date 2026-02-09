91653236472645423fa1f231810096510a05997a
jamesszymczak <jamesszymczak@tamu.edu>
2026-02-09 10:45:07 -0600

    Update analysis_terrain_slope.py

--- analysis_terrain_slope.py
+++ analysis_terrain_slope.py
@@ -3,6 +3,7 @@ from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 import subfunctions as sf
 
+#function that gets fed to the root finder
 def f(o, ta, r, p, Crr = 0.15):
     return sf.F_net(np.array([o]), np.array([ta]), r, p, Crr)
 
@@ -17,13 +18,13 @@ slope_array_deg = np.linspace(-15,35,25)
 v_max = np.ndarray(25)
 wheel_radius = sf.Marvin["rover"]["wheel_assembly"]["wheel"]["radius"]
 
-
+#finds v_max at each slope
 for i in range(25):
     if f(0, slope_array_deg[i],rover, planet) * f(3.8, slope_array_deg[i],rover, planet) >= 0:
         v_max[i] = motor['speed_noload'] * wheel_radius
     else:
         v_max[i] = root_scalar(f,method='bisect', args=(slope_array_deg[i],rover, planet), bracket=[0,motor['speed_noload']]).root * wheel_radius
-print(v_max)
+
 
 plt.plot(slope_array_deg, v_max)
 plt.xlabel('Terrain Angle (degrees)')
