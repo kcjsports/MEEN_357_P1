670f73dd777377f02b83ad6f4ea71766016a1b18
jamesszymczak <jamesszymczak@tamu.edu>
2026-02-09 22:41:46 -0600

    Update analysis_combined_terrain.py

--- analysis_combined_terrain.py
+++ analysis_combined_terrain.py
@@ -2,27 +2,41 @@ import numpy as np
 from scipy.optimize import root_scalar
 import matplotlib.pyplot as plt
 import subfunctions as sf
+from mpl_toolkits.mplot3d import Axes3D
 
 #function that gets fed to the root finder
-def f(o, ta, r, p, Crr = 0.15):
+def f(o, ta, r, p, Crr):
     return sf.F_net(np.array([o]), np.array([ta]), r, p, Crr)
 
 #Imported variables
 motor = sf.Marvin["rover"]["wheel_assembly"]["motor"]
 rover = sf.Marvin["rover"]
 planet = sf.Marvin["planet"]
+wheel_radius = sf.Marvin["rover"]["wheel_assembly"]["wheel"]["radius"]
 #variables
-omega = np.linspace(0, motor['speed_noload'], 25) #array of our omega 
 Crr_array = np.linspace(0.01,0.5,25)
 slope_array_deg = np.linspace(-15,35,25)
-v_max = np.ndarray(25)
-wheel_radius = sf.Marvin["rover"]["wheel_assembly"]["wheel"]["radius"]
+CRR, SLOPE = np.meshgrid(Crr_array, slope_array_deg)
+VMAX = np.zeros(np.shape(CRR), dtype = float)
 
 #finds v_max at each slope
-for i in range(25):
-  for j in range(25):
+N = np.shape(CRR)[0]
+for i in range(N):
+ for j in range(N):
+    Crr_sample = float(CRR[i,j])
+    slope_sample = float(SLOPE[i,j])
     if f(0, slope_array_deg[i],rover, planet, Crr_array[j]) * f(3.8, slope_array_deg[i],rover, planet, Crr_array[j]) >= 0:
-        v_max[i] = motor['speed_noload'] * wheel_radius
+       VMAX[i,j] = float('NaN')
     else:
-        v_max[i] = root_scalar(f,method='bisect', args=(slope_array_deg[i],rover, planet, Crr_array[j]), bracket=[0,motor['speed_noload']]).root * wheel_radius
+       VMAX[i,j] = root_scalar(f,method='bisect', args=(slope_array_deg[i],rover, planet, Crr_array[j]), bracket=[0,motor['speed_noload']]).root * wheel_radius
 
+
+figure = plt.figure()
+ax = figure.add_subplot(projection='3d')
+ax.plot_surface(CRR, SLOPE, VMAX)
+ax.view_init(27, -10)
+ax.set_xlabel("Coefficent of rolling resistance")
+ax.set_ylabel('Terrain Angle')
+ax.set_zlabel('Max Velocity (m/s)')
+plt.title('Maximum Velocity at Different \n Terrain Angles and Coefficent of Rolling Resistance')
+plt.show()
